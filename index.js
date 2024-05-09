d19219ec0dd619fa02d4defe0a2e3a172b0d2495
candourist <sajadh.ismael@gmail.com>
2024-05-09 15:35:49 +0300

    Completed assignment - YaY!

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+function superbowlWin(record) {
+    
+    const win = record.find(
+    //silly mistake, I didn't add "const".
+        function (obj)
+        //another silly mistake is I forgot to add (obj)
+        {
+    
+            return obj.result === "W"; }
+            //my mistake is I added ";" here... I fixed it.
+    )
+    
+    return win ? win.year : undefined;
+
+} 
